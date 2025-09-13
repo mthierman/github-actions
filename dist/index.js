5eb5ab9dbbd9aefd279d7e179c2c223b48e70496
mthierman <mthierman@gmail.com>
2025-09-12 18:01:06 -0600

    External packages test

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
+// src/index.js
+var core = __toESM(require("@actions/core"), 1);
+var github = __toESM(require("@actions/github"), 1);
+try {
+  const nameToGreet = core.getInput("who-to-greet");
+  core.info(`Hello ${nameToGreet}!`);
+  const time = (/* @__PURE__ */ new Date()).toTimeString();
+  core.setOutput("time", time);
+  const payload = JSON.stringify(github.context.payload, void 0, 2);
+  core.info(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
