7c95bc40539310eb729461103a88812a4242b948
mthierman <mthierman@gmail.com>
2025-10-28 14:33:04 -0600

    Use esm

--- build.ts
+++ build.ts
@@ -4,7 +4,7 @@ import { resolve } from "node:path";
 await esbuild.build({
     platform: "node",
     target: "node24",
-    format: "cjs",
+    format: "esm",
     entryPoints: [resolve("notify-web/action.ts")],
     outfile: resolve("notify-web/dist/action.ts"),
     bundle: true,
