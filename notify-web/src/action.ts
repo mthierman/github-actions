f1d70f63e0d14e6690a34d459b4b39db5848d6a7
mthierman <mthierman@gmail.com>
2025-10-28 13:40:44 -0600

    Update build locations

--- notify-web/src/action.ts
+++ notify-web/src/action.ts
@@ -1,3 +1,6 @@
+// https://docs.github.com/en/actions/tutorials/create-actions/create-a-javascript-action
+// https://docs.github.com/en/actions/reference/workflows-and-actions/metadata-syntax#runs
+// https://github.com/actions/toolkit
 import * as core from "@actions/core";
 import { readFileSync } from "node:fs";
 
