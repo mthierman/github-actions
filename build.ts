39b60286b247f79a586f6dc5b2c7462a0a42e14b
mthierman <mthierman@gmail.com>
2025-10-28 13:45:14 -0600

    Remove hello-js and testing-js

--- build.ts
+++ build.ts
@@ -2,28 +2,10 @@ import esbuild from "esbuild";
 import { resolve } from "node:path";
 
 await esbuild.build({
-    entryPoints: [resolve("hello-js/src/action.ts")],
-    bundle: true,
-    platform: "node",
-    target: "node24",
-    format: "cjs",
-    outfile: resolve("hello-js/action.cjs"),
-});
-
-await esbuild.build({
-    entryPoints: [resolve("testing-js/src/action.ts")],
-    bundle: true,
-    platform: "node",
-    target: "node24",
-    format: "cjs",
-    outfile: resolve("testing-js/action.cjs"),
-});
-
-await esbuild.build({
     entryPoints: [resolve("notify-web/src/action.ts")],
     bundle: true,
     platform: "node",
     target: "node24",
     format: "cjs",
-    outfile: resolve("notify-web/action.cjs"),
+    outfile: resolve("notify-web/dist/action.cjs"),
 });
