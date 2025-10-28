f8a369851a1ddaf1790ee7f8abce36a18e5a6bee
mthierman <mthierman@gmail.com>
2025-10-28 07:50:59 -0600

    Testing-js

---
+++ testing-js/action.ts
@@ -0,0 +1,14 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+try {
+    const package_json = core.getInput("package-json");
+    core.info(`${package_json}`);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    core.info(`The event payload: ${payload}`);
+} catch (error) {
+    const err = error as Error;
+    core.setFailed(err.message);
+}
