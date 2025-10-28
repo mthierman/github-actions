c93ff216a8a0c61205467d25e8bddf810a134a11
mthierman <mthierman@gmail.com>
2025-10-28 13:43:47 -0600

    Update deps

--- notify-web/src/action.ts
+++ notify-web/src/action.ts
@@ -3,6 +3,10 @@
 // https://github.com/actions/toolkit
 import * as core from "@actions/core";
 import { readFileSync } from "node:fs";
+// import { Temporal } from "@js-temporal/polyfill";
+// import { Octokit } from "@octokit/rest";
+// import { spawnSync } from "node:child_process";
+// import package_json from "../package.json" with { type: "json" };
 
 try {
     const package_json = JSON.parse(readFileSync("package.json", { encoding: "utf8" }));
