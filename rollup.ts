735a686e17265fb95ad5810d857ecac3050be35b
mthierman <mthierman@gmail.com>
2025-10-28 14:52:28 -0600

    Fix index

--- rollup.ts
+++ rollup.ts
@@ -5,7 +5,7 @@ import { resolve } from "node:path";
 import { rollup } from "rollup";
 
 const bundle = await rollup({
-    input: resolve("notify-web/index.ts"),
+    input: resolve("notify-web/src/index.ts"),
     output: { esModule: true, file: "index.js", format: "es", sourcemap: true },
     plugins: [commonjs(), nodeResolve({ preferBuiltins: true }), typescript()],
 });
