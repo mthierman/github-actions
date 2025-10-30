877b33008a22c8a842daf0595490046193bf46fb
mthierman <mthierman@gmail.com>
2025-10-29 18:29:26 -0600

    Update build script, remove source maps

--- build.ts
+++ build.ts
@@ -2,32 +2,28 @@ import { build } from "esbuild";
 import { builtinModules } from "node:module";
 import { resolve } from "node:path";
 
-async function build_actions() {
-    const actions = [
-        { name: "notify-web" },
-        { name: "receive-web" },
-        { name: "install-innosetup" },
-        { name: "install-ninja" },
-    ];
+const actions = [
+    { name: "notify-web" },
+    { name: "receive-web" },
+    { name: "install-innosetup" },
+    { name: "install-ninja" },
+];
 
-    await Promise.all(
-        actions.map((action) =>
-            build({
-                entryPoints: [resolve(`${action.name}/src/index.ts`)],
-                outfile: resolve(`${action.name}/dist/index.cjs`),
-                platform: "node",
-                target: "node24",
-                format: "cjs",
-                bundle: true,
-                minify: false,
-                sourcemap: false,
-                external: [...builtinModules],
-            }),
-        ),
-    );
-}
-
-build_actions().catch((err) => {
-    console.error(err);
+await Promise.all(
+    actions.map((action) =>
+        build({
+            entryPoints: [resolve(`${action.name}/src/index.ts`)],
+            outfile: resolve(`${action.name}/dist/index.cjs`),
+            platform: "node",
+            target: "node24",
+            format: "cjs",
+            bundle: true,
+            minify: false,
+            sourcemap: false,
+            external: [...builtinModules],
+        }),
+    ),
+).catch((error) => {
+    console.error(error);
     process.exit(1);
 });
