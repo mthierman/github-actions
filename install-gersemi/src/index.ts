836c590dc4b5c62c55dc64d1821f7b4bb1685bce
mthierman <mthierman@gmail.com>
2025-10-29 22:43:55 -0600

    Try to fix uv tool caches

--- install-gersemi/src/index.ts
+++ install-gersemi/src/index.ts
@@ -3,13 +3,15 @@ import * as core from "@actions/core";
 import { getInput } from "@actions/core";
 import { execSync, spawnSync } from "node:child_process";
 import * as os from "node:os";
+import * as path from "node:path";
 
 (async () => {
     try {
         const version = getInput("version", { required: true });
         const tool_dir = execSync("uv tool dir", { encoding: "utf8" }).trim();
+        const bin_dir = path.join(os.homedir(), ".local", "bin");
         const cache_key = `${os.platform()}-gersemi-${version}`;
-        const restored_key = await cache.restoreCache([tool_dir], cache_key);
+        const restored_key = await cache.restoreCache([tool_dir, bin_dir], cache_key);
 
         if (!restored_key) {
             const install = spawnSync("uv", ["tool", "install", `gersemi==${version}`], {
@@ -20,10 +22,10 @@ import * as os from "node:os";
                 throw new Error(`uv tool install failed: ${install.status}`);
             }
 
-            await cache.saveCache([tool_dir], cache_key);
+            await cache.saveCache([tool_dir, bin_dir], cache_key);
         }
 
-        core.addPath(tool_dir);
+        core.addPath(bin_dir);
         core.info(`Gersemi ${version} added to PATH`);
     } catch (error: any) {
         core.setFailed(error.message);
